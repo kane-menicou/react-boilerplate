11801ee24853626fb76e46dea30ebd57979e4332
Kane Menicou <kanemenicou@gmail.com>
2019-05-05 15:27:22 +0100

    Updates demo to match linter styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import * as React from 'react'
-import {hot} from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 import style from './App.sass'
 import logo from './logo.png'
-import {useState} from "react";
 
 const App = () => {
-    const [name, setName] = useState('');
+  const [name, setName] = React.useState('')
 
-    function onChange({currentTarget}) {
-        setName(currentTarget.value)
-    }
+  function onChange ({ currentTarget }: React.ChangeEvent<HTMLInputElement>) {
+    setName(currentTarget.value)
+  }
 
-    return (
-        <div>
-            <img className={style.image} src={logo}/>
-            <h1 className={`${style.header} header`}>React App</h1>
-            <p className={style.body}>Hi {name.trim() === '' ? '' : `${name}!`}</p>
-            <input type="text" placeholder='Name' value={name} onChange={onChange}/>
-        </div>
-    )
+  return (
+    <div>
+        <img alt='React logo' className={style.image} src={logo}/>
+        <h1 className={`${style.header} header`}>React App</h1>
+        <p className={style.body}>Hi {name.trim() === '' ? '' : `${name}!`}</p>
+        <input type='text' placeholder='Name' value={name} onChange={onChange}/>
+    </div>
+  )
 }
 
 export default hot(module)(App)
