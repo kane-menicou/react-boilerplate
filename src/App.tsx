3caa09ba7378c076d3112b018dad568b15eb27ac
Kane Menicou <kanemenicou@gmail.com>
2019-05-05 15:15:01 +0100

    Changes App example to contain hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react'
-import { hot } from 'react-hot-loader'
+import {hot} from 'react-hot-loader'
 import style from './App.sass'
 import logo from './logo.png'
+import {useState} from "react";
 
 const App = () => {
-  return (
-    <div>
-      <img className={style.image} src={logo}/>
-      <h1 className={`${style.header} header`}>React App</h1>
-      <p className={style.body}>React app boiler plate. Ready for use.</p>
-    </div>
-  )
+    const [name, setName] = useState('');
+
+    function onChange({currentTarget}) {
+        setName(currentTarget.value)
+    }
+
+    return (
+        <div>
+            <img className={style.image} src={logo}/>
+            <h1 className={`${style.header} header`}>React App</h1>
+            <p className={style.body}>Hi {name.trim() === '' ? '' : `${name}!`}</p>
+            <input type="text" placeholder='Name' value={name} onChange={onChange}/>
+        </div>
+    )
 }
 
 export default hot(module)(App)
